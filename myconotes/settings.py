ca6ce42db0f938e7a216ffa43c6a9e9ea325a1ce
Taranjeet <reachtotj@gmail.com>
2015-01-24 20:05:27 +0530

    settings commit

--- myconotes/settings.py
+++ myconotes/settings.py
@@ -14,8 +14,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 ADMINS = (
     ('Taranjeet Singh', 'reachtotj@gmail.com'),
-  
-)
+  )
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
